94ab8686008eb4bfd0836d90c8259e1b8ac43367
Carlos Matos <carlosarturmatos1977@gmail.com>
2025-03-27 12:55:45 +0000

    JWT and tokens enhancement and fix.

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -16,6 +16,18 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
+    public function save(User $user): void
+    {
+        $this->getEntityManager()->persist($user);
+        $this->getEntityManager()->flush();
+    }
+
+    public function remove(User $user): void
+    {
+        $this->getEntityManager()->remove($user);
+        $this->getEntityManager()->flush();
+    }
+
     //    /**
     //     * @return User[] Returns an array of User objects
     //     */
